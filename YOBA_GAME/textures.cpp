38726acaebd24a077a03dc1239b0b5406bfd87c2
Cryostasis <argonhero@gmail.com>
2014-12-01 18:36:54 +1000

    Minor changes

--- YOBA_GAME/textures.cpp
+++ YOBA_GAME/textures.cpp
@@ -61,15 +61,11 @@ GLuint get_texture_from_tga(char *name)
 
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-<<<<<<< HEAD
+
 	//glTexImage2D(GL_TEXTURE_2D, 0, internalFormat, header->width, header->height, 0, format,
 	//	GL_UNSIGNED_BYTE, (const GLvoid*)(buffer + sizeof(TGAHeader)+header->idlength));
 
 	gluBuild2DMipmaps(GL_TEXTURE_2D, internalFormat, header->width, header->height, format,
-=======
-
-	glTexImage2D(GL_TEXTURE_2D, 0, internalFormat, header->width, header->height, 0, format,
->>>>>>> parent of 551be9f... Minor changes
 		GL_UNSIGNED_BYTE, (const GLvoid*)(buffer + sizeof(TGAHeader)+header->idlength));
 
 	delete[] buffer;
